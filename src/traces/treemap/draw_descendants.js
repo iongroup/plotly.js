251c3b236c12cbfbd954296f9de8dc4e1325cad4
Chirag-Khanna <chirag.khanna@iongroup.com>
2021-05-31 16:32:41 +0530

    removed arrow function for IE
    (cherry picked from commit 8ef91acf5014f1afef43ba3066277560accd658a)

--- src/traces/treemap/draw_descendants.js
+++ src/traces/treemap/draw_descendants.js
@@ -50,10 +50,16 @@ module.exports = function drawDescendants(gd, cd, entry, slices, opts) {
         flipY: trace.tiling.flip.indexOf('y') > -1,
         pad: {
             inner: trace.tiling.pad,
-            top:  !hasBottom ? ((node) => (node.depth === 0 && !helpers.getPtLabel(node))? trace.marker.pad.r: trace.marker.pad.t) : trace.marker.pad.t, // do not add large padding when label not present for root level
+            top:  hasBottom ? trace.marker.pad.t : // do not add large padding when label not present for root level
+                function(node) { 
+                    return ((node.depth === 0 && !helpers.getPtLabel(node))? trace.marker.pad.r: trace.marker.pad.t);
+                },
             left: trace.marker.pad.l,
             right: trace.marker.pad.r,
-            bottom: hasBottom ? ((node) => (node.depth === 0 && !helpers.getPtLabel(node))? trace.marker.pad.r: trace.marker.pad.b) : trace.marker.pad.b // do not add large padding when label not present for root level
+            bottom: !hasBottom ? trace.marker.pad.b : // do not add large padding when label not present for root level
+                function (node) { 
+                    return ((node.depth === 0 && !helpers.getPtLabel(node))? trace.marker.pad.r: trace.marker.pad.b);
+                }
         }
     });
 
