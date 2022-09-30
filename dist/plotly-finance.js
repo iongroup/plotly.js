31861fc9e0aa64ac3e55978ff12eb7d0b4968f7d
lakshit.dua <lakshit.dua@iongroup.com>
2022-09-30 16:52:43 +0530

    - New build 1.56-ion10

--- dist/plotly-finance.js
+++ dist/plotly-finance.js
@@ -26784,6 +26784,20 @@ module.exports = function draw(gd, opts) {
     var legend = Lib.ensureSingle(layer, 'g', 'legend', function(s) {
         if(opts._main) s.attr('pointer-events', 'all');
     });
+    // create legend as a popup in case of legend being adjusted to fit within the graph
+    var legendPopupAnchor = Lib.ensureSingle(layer, 'g', 'legend-popup-anchor', function(s) {
+        s.attr('pointer-events', 'all').attr('title', 'Show Legend');
+        // add path for the icon
+        var anchorIcon = Lib.ensureSingle(s, 'path');
+        anchorIcon.attr('d', 'M255.998,90.001c91.74,0,166.002,74.241,166.002,165.998c0,91.741-74.245,166-166.002,166c-91.74,0-165.998-74.243-165.998-166C90,164.259,164.243,90.001,255.998,90.001 M255.998,50.001C142.229,50.001,50,142.229,50,255.999c0,113.771,92.229,206,205.998,206c113.771,0,206.002-92.229,206.002-206C462,142.229,369.77,50.001,255.998,50.001L255.998,50.001z M285.822,367.567h-57.646V230.6h57.646V367.567z M257,202.268c-17.522,0-31.729-14.206-31.729-31.73c0-17.522,14.206-31.729,31.729-31.729c17.524,0,31.728,14.206,31.728,31.729C288.728,188.062,274.524,202.268,257,202.268z');
+    });
+    // transform the anchor icon to scale to required size and be fixed to top right position
+    legendPopupAnchor.attr("transform", `scale(0.03, 0.03) translate(${(fullLayout.width - 15) * 32}, -50)`);
+    // Create rect as the icon bg for proper onClick event registration
+    var anchorIconBg = Lib.ensureSingle(legendPopupAnchor, 'rect');
+    const legendPopupAnchorBox = Drawing.bBox(legendPopupAnchor.node());
+    const [legendPopupAnchorHeight, legendPopupAnchorWidth] = [legendPopupAnchorBox.height, legendPopupAnchorBox.width];
+    anchorIconBg.attr('x', 0).attr('y', '0').attr('height', legendPopupAnchorHeight).attr('width', legendPopupAnchorWidth).style('fill', 'none').style('fill-opacity', '1');
 
     var clipPath = Lib.ensureSingleById(fullLayout._topdefs, 'clipPath', clipId, function(s) {
         s.append('rect');
@@ -26855,21 +26869,47 @@ module.exports = function draw(gd, opts) {
             if(opts._main && fullLayout.margin.autoexpand) {
                 var lx0 = lx;
                 var ly0 = ly;
+                var hideLegendInPopup;
 
                 lx = Lib.constrain(lx, 0, fullLayout.width - opts._width);
                 ly = Lib.constrain(ly, 0, fullLayout.height - opts._effHeight);
 
                 if(lx !== lx0) {
                     Lib.log('Constrain legend.x to make legend fit inside graph');
+                    hideLegendInPopup = true;
                 }
                 if(ly !== ly0) {
                     Lib.log('Constrain legend.y to make legend fit inside graph');
+                    hideLegendInPopup = true;
                 }
             }
 
             // Set size and position of all the elements that make up a legend:
             // legend, background and border, scroll box and scroll bar as well as title
-            if(opts._main) Drawing.setTranslate(legend, lx, ly);
+            if(opts._main && !hideLegendInPopup) {
+                legend.style('display', 'block');
+                legend.classed('show-in-popup', false);
+                legendPopupAnchor.style('display', 'none');
+                Drawing.setTranslate(legend, lx, ly);
+            }
+            if(opts._main && hideLegendInPopup) {
+                // Hide legend and remove in-popup class
+                legend.style('display', 'none');
+                legend.classed('show-in-popup', false);
+                // Show popup anchor, assign click handler
+                legendPopupAnchor.style('display', 'block');
+                legendPopupAnchor.on('click', toggleLegendPopup.bind(this));
+                // translate the legend relative to the popupAnchor
+                Drawing.setTranslate(legend, fullLayout.width - opts._width, legendPopupAnchorHeight * 0.03);
+            }
+
+            function toggleLegendPopup() {
+                if (legend.classed('show-in-popup') && legend.style('display') !== 'none') {
+                    legend.classed('show-in-popup', false).style('display', 'none');
+                } else {
+                    legend.classed('show-in-popup', true).style('display', 'block');
+                }
+            }
 
             // to be safe, remove previous listeners
             scrollBar.on('.drag', null);
@@ -87350,7 +87390,7 @@ module.exports = {
 'use strict';
 
 // package version injected by `npm run preprocess`
-exports.version = '1.56.0-ion9';
+exports.version = '1.56.0-ion10';
 
 },{}]},{},[8])(8)
 });
--- dist/plotly-geo-assets.js
+++ dist/plotly-geo-assets.js
@@ -78,7 +78,7 @@ exports.topojson = saneTopojson;
 'use strict';
 
 // package version injected by `npm run preprocess`
-exports.version = '1.56.0-ion9';
+exports.version = '1.56.0-ion10';
 
 },{}]},{},[16])(16)
 });
