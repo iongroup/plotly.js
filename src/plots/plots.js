64af83d28ec6072cfdec58aae05d21379bd090e4
Deepak Jain <deepak.jain@iongroup.com>
2024-01-23 15:49:23 +0530

    Merge conflict fix.

--- src/plots/plots.js
+++ src/plots/plots.js
@@ -2024,8 +2024,6 @@ plots.doAutoMargin = function(gd) {
     }
     const extraRMargin = 4;
     const extraBMargin = 5;
-    const extraRMargin = 4;
-    const extraBMargin = 5;
     gs.l = Math.round(ml);
     gs.r = Math.round(mr) + extraRMargin;
     gs.t = Math.round(mt);
